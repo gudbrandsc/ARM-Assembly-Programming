3759f00df037f078a3fd908587e9ac21cdc9eba0
gudbrandsc <gudbrand.schistad@gmail.com>
2018-03-05 19:57:03 +0000

    Forgot to add find_str_c file

---
+++ find_str_c.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h> 
+int find_str_c(char* string, char* sub){
+  int j,k,res;
+  int i = 0;
+  int string_length = strlen(string);
+  int sub_length = strlen(sub);
+  
+  while(string[i] != '\0'){ // While not end of array
+    if(string[i] == sub[0]){
+      j = i + 1; // start from i + 1 
+      k = 1; // Start k from 1
+      res = i; // Return index
+      while(string[j] == sub[k]){ 
+	if(k == sub_length-1){ // If last value of substring
+	  return res;
+	}
+	j += 1;
+	k += 1;
+      }      
+    }
+    i += 1;
+  }
+  return -1;
+}
