9b26290e0a76122091698da9a70576aa46c356b0
gudbrandsc <gudbrand.schistad@gmail.com>
2018-03-06 05:44:42 +0000

    Fixed code indentation

--- driver.c
+++ driver.c
@@ -38,16 +38,16 @@ void sum_array_test(int* array, int size){
 
   
   if(size > 10){ // if array size is greater than 10 print only start and end values
-    printf("sum_array_c({%d,%d,%d,...,%d},%d) = %d\n", array[0], array[1], array[2], array[size-1], size,sum_c);
-    printf("sum_array_s({%d,%d,%d,...,%d},%d) = %d\n", array[0], array[1], array[2], array[size-1], size,sum_s);
+    printf("sum_array_c({%d,%d,%d,...,%d}, %d) = %d\n", array[0], array[1], array[2], array[size-1], size,sum_c);
+    printf("sum_array_s({%d,%d,%d,...,%d}, %d) = %d\n", array[0], array[1], array[2], array[size-1], size,sum_s);
   }else{ // Print all values in array
     printf("sum_array_c({");
     print_values(array, size);
-    printf("},%d) = %d\n", size, sum_c);
+    printf("}, %d) = %d\n", size, sum_c);
     
     printf("sum_array_s({");
     print_values(array, size);
-    printf("},%d) = %d\n", size, sum_s);
+    printf("}, %d) = %d\n", size, sum_s);
   }
 }
 
@@ -57,16 +57,16 @@ void find_max_test(int* array, int size){
   int max_s =  find_max_s(array, size);
   
   if(size > 10){
-    printf("find_max_c({%d,%d,%d,...,%d},%d) = %d\n", array[0], array[1], array[2], array[size-1], size, max_c);
-    printf("find_max_s({%d,%d,%d,...,%d},%d) = %d\n", array[0], array[1], array[2], array[size-1], size, max_s);
+    printf("find_max_c({%d,%d,%d,...,%d}, %d) = %d\n", array[0], array[1], array[2], array[size-1], size, max_c);
+    printf("find_max_s({%d,%d,%d,...,%d}, %d) = %d\n", array[0], array[1], array[2], array[size-1], size, max_s);
   }else{
     printf("find_max_c({");
     print_values(array, size);
-    printf("},%d) = %d\n", size, max_c);
+    printf("}, %d) = %d\n", size, max_c);
     
     printf("find_max_s({");
     print_values(array, size);
-    printf("},%d) = %d\n", size, max_s);
+    printf("}, %d) = %d\n", size, max_s);
   }
 }
 
@@ -74,19 +74,19 @@ void find_max_test(int* array, int size){
 //Function that call's test for summing array, for each array  
 void run_sum_array_tests(int* array1, int* array2, int* array3, int* array4){
   printf("====== Testing summing array ======\n");
-  sum_array_test(array1,10);
-  sum_array_test(array2,10);
-  sum_array_test(array3,10);
-  sum_array_test(array4,2000);
+  sum_array_test(array1, 10);
+  sum_array_test(array2, 10);
+  sum_array_test(array3, 10);
+  sum_array_test(array4, 2000);
 }
 
 //Function that call's test for finding the max value of  each array
 void run_find_max_tests(int* array1, int* array2, int* array3, int* array4){
   printf("\n====== Testing find max number ======\n");
-  find_max_test(array1,10);
-  find_max_test(array2,10);
-  find_max_test(array3,10);
-  find_max_test(array4,2000);
+  find_max_test(array1, 10);
+  find_max_test(array2, 10);
+  find_max_test(array3, 10);
+  find_max_test(array4, 2000);
 }
 
 // Function to print all n fibonacci sequence numbers from iterativ functions
@@ -119,7 +119,6 @@ void run_fib_rec_test(int n){
   int i;
 
   printf("\n====== Testing recursively fibonacci sequence to: %d ======\n", n);
-
   printf("fib_rec_c: ");
   for(i = 1; i <= n; i++){ // Print n fib numbers from recursive c function
     if(i == n){
@@ -141,8 +140,8 @@ void run_fib_rec_test(int n){
 
 // Function that call's find_str_* and prints the result
 void find_sub_string_test(char* string, char *substring){
-  printf("find_str_c(%s,%s): %d \n", string, substring, find_str_c(string,substring));
-  printf("find_str_s(%s,%s): %d \n", string, substring, find_str_s(string,substring));
+  printf("find_str_c(%s, %s): %d \n", string, substring, find_str_c(string, substring));
+  printf("find_str_s(%s, %s): %d \n", string, substring, find_str_s(string, substring));
 }
 // Function to run all test's to find a substring
 void run_find_substring_tests(char* string1, char* substring1, char* string2, char* substring2, char* substring3){
@@ -165,10 +164,10 @@ int main(int argc, char* argv){
   char string2[] = {" "};
   char substring3[] = {" "};
   
-  populate_large_array(array4,2000); // Populate array 4 with values from 1-2000 
+  populate_large_array(array4, 2000); // Populate array 4 with values from 1-2000 
 
-  run_sum_array_tests(array1,array2,array3,array4); // Test to find sum of all the arrays
-  run_find_max_tests(array1,array2,array3,array4); // Test to find max value for all arrays
+  run_sum_array_tests(array1, array2, array3, array4); // Test to find sum of all the arrays
+  run_find_max_tests(array1, array2, array3, array4); // Test to find max value for all arrays
   run_fib_iter_test(20); // Test first 20 fibonacci fibonacci sequence numbers
   run_fib_rec_test(20); // Test first 20 fibonacci fibonacci sequence numbers
   run_find_substring_tests(string1, substring1, string2, substring2, substring3);
